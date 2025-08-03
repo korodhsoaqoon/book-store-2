a50b885ab666b5b45c59a918ffb76e6b52180e81
Abdillahi Haji <hajiabdi213@gmail.com>
2025-08-03 07:10:21 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import dotenv from "dotenv";
+import { connectDB } from "./config/database.js";
+import bookRoute from "./routes/book.route.js";
+import cors from "cors";
+import path from "path";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.use("/api/books", bookRoute);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
